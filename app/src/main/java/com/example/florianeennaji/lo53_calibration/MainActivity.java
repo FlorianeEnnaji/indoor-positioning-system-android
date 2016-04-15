292efa75b13bd1f66aeae7d26e0fa7835b7127cb
Floriane Ennaji <ennajif@gmail.com>
2016-04-15 18:29:02 +0200

    added wifi scan

--- app/src/main/java/com/example/florianeennaji/lo53_calibration/MainActivity.java
+++ app/src/main/java/com/example/florianeennaji/lo53_calibration/MainActivity.java
@@ -4,9 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void startCalibration(View v) {
-        Log.d("00", "Start calibration");
+        this.startActivity(new Intent(this, WiFiActivity.class));
+        this.finish();
     }
 }
